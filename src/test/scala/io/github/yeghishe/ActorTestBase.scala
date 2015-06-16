3fdfbbd790547656ebf9e0f3fe4c0de1e4cd2b85
Yeghishe Piruzyan <ypiruzyan@gmail.com>
2015-06-15 17:19:16 -0700

    removing comments

--- src/test/scala/io/github/yeghishe/ActorTestBase.scala
+++ src/test/scala/io/github/yeghishe/ActorTestBase.scala
@@ -4,9 +4,6 @@ import akka.actor.ActorSystem
 import akka.testkit.{ TestKit, ImplicitSender }
 import org.scalatest.{ BeforeAndAfterAll, WordSpecLike, Matchers }
 
-/**
- * Created by yeghishe on 6/15/15.
- */
 abstract class ActorTestBase extends TestKit(ActorSystem())
     with ImplicitSender
     with WordSpecLike
